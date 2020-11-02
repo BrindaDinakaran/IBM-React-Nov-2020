7acd6795e2f91397256642655e9158b4af1557b3
BrindaDinakaran <brindadina@in.ibm.com>
2020-11-02 16:25:50 +0530

    constructor function for spinner

---
+++ Assignment-2/spinner-constructor.js
@@ -0,0 +1,15 @@
+function Spinner(){
+	this.counter = 0;
+	this.up = function(){
+		return ++this.counter;
+	}
+	
+	this.down = function(){
+		return --this.counter;
+	}
+}
+
+var spin = new Spinner()
+console.log(spin.up())
+console.log(spin.up())
+console.log(spin.down())
